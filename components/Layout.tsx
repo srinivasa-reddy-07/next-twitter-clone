43c14ec58eebc78c97a639779c3114e50485a8bb
srinivasa-reddy-07 <srinivasareddykapa18@gmail.com>
2024-04-10 16:37:04 +0530

    Changed the title

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import Head from "next/head"
 import FollowBar from "@/components/layout/FollowBar"
 import Sidebar from "@/components/layout/Sidebar"
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="h-screen bg-black">
+      <Head>
+        <title>Twitter Clone</title>
+      </Head>
       <div className="container h-full mx-auto xl:px-30 max-w-6xl">
         <div className="grid grid-cols-4 h-full">
           <Sidebar />
